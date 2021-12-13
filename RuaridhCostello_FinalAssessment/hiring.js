a12d74c935df68d25c2c1edd94e3fdf95b13822f
ruaridhcostello <84516993+ruaridhcostello@users.noreply.github.com>
2021-12-13 17:25:59 -0500

    Add files via upload

---
+++ RuaridhCostello_FinalAssessment/hiring.js
@@ -0,0 +1,12 @@
+function dynamic()
+{
+if(document.getElementById('hiring').checked)
+{
+document.getElementById('rate').style.display = "block";
+}
+else
+{
+document.getElementById('rate').style.display = "none";
+}
+return;
+}
